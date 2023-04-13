c8b0044f6dde48631ecdc41f7aa8ce1c14e8a613
Ceri Dorsey-Tyler <cerimay@gmail.com>
2023-04-12 21:58:16 -0400

    changes

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -26,6 +26,14 @@ let gotImages = [
   'https://i.imgur.com/cLc2u8s.png',
 ]
 
+let proImages = [
+  'https://i.imgur.com/SO3KVu7.png',
+  'https://i.imgur.com/BBI0IWJ.png',
+  'https://i.imgur.com/mtA3rcT.png',
+  'https://i.imgur.com/fwotGWw.png',
+  'https://i.imgur.com/FDnsdmr.png',
+]
+
 function ProjectContainer() {
   return (
     <div class="bg-gray-100 min-h-screen flex flex-row flex-wrap justify-center">
@@ -201,6 +209,64 @@ function ProjectContainer() {
               <p class="text-sm font-medium text-gray-700 mb-2">Tech Used:</p>
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                 React
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-wrap justify-center items-col ">
+        <div class="max-w-fit rounded overflow-hidden shadow-lg w-full max-w-xl m-10">
+          <div class="relative">
+            <Carousel
+              showArrows={true}
+              showStatus={false}
+              showThumbs={false}
+              infiniteLoop={true}
+              style={{ maxHeight: 'calc(100vh - 20rem)' }}>
+              {proImages.map((image, id) => (
+                <div key={id}>
+                  <img
+                    src={image}
+                    alt="Website Images"
+                    style={{ maxHeight: 'calc(100vh - 4rem)' }}
+                  />
+                </div>
+              ))}
+            </Carousel>
+            <div class="absolute top-0 left-0 right-0 h-20"></div>{' '}
+          </div>
+
+          <div class="px-6 py-4">
+            <div class="font-bold text-xl mb-2">Wander</div>
+            <p class="text-gray-700 text-base">
+            This portfolio showcases my projects and achievements, providing a glimpse into my skills and work style.
+            </p>
+            <div class="py-4 flex">
+              <div class="mr-4">
+                <p class="text-sm font-medium text-gray-700 mb-2"></p>
+                <a
+                  href="https://github.com/ohheycerimay/Portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="text-gray-900 hover:bg-gray-100 duration-300">
+                  <FontAwesomeIcon icon={faGithub} /> Github
+                </a>
+              </div>
+              {/* <div>
+                <p class="text-sm font-medium text-gray-700 mb-2"></p>
+                <a
+                  href="/*"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="text-gray-900 hover:bg-gray-100 duration-300">
+                  <FontAwesomeIcon icon={faVideo} /> Demo
+                </a>
+              </div> */}
+            </div>
+            <div class="py-4">
+              <p class="text-sm font-medium text-gray-700 mb-2">Tech Used:</p>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                React, Tailwind
               </span>
             </div>
           </div>
